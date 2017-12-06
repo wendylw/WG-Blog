84fb4256c084e15b6d62a834a4ae2465b3299f5f
yvonneyang <yvongyang@163.com>
2017-12-06 23:15:43 +0800

    add animation for first page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 $(function(){
-    $('#fullpage').fullpage(
-        {
-            // sectionsColor: ['orange', 'blue', 'red'],
-            navigation: true
-        }
-    );
+    $('#fullpage').fullpage({
+        navigation: true
+    });
+
     $(document).foundation();
 
+    // Animation for intro-w
     $('.intro-g .button').click(function() {
        $('.intro-w').addClass('show-all');
+        $('.intro-w .button').addClass('back');
+
+        $('.back').click(function() {
+            $('.intro-w').removeClass('show-all');
+            $('.intro-g').removeClass('show-all');
+            $(this).removeClass('back');
+        });
+    });
+
+    // Animation for intro-g
+    $('.intro-w .button').click(function() {
+        $('.intro-g').addClass('show-all');
+        $('.intro-g .button').addClass('back');
+
+        $('.back').click(function() {
+            $('.intro-w').removeClass('show-all');
+            $('.intro-g').removeClass('show-all');
+            $(this).removeClass('back');
+        });
     });
 });
